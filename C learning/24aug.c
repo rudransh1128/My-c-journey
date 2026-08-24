38279a9c8c2b6c0227043612a229fa37c8503514
Rudransh Gupta <rudranshgupta11728@gmail.com>
2026-08-24 22:22:52 +0530

    learnt something new

---
+++ C learning/24aug.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    printf("hello this is rudransh gupta \t \t lol");
+// \t is used for leaving spcaing similar to tab
+    printf("hello world this is rudrnash \n");
+    // \n is used for changing code to next line. so like if i used it in an input it would print line and take input in the next line rather then taking it in the same 
+    printf("and \a is used to crate a windows sound ");
+
+
+
+}
