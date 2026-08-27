1b9c5ec9f7c1acf0f8826b357d21a26cc9c0a791
Rudransh Gupta <rudranshgupta11728@gmail.com>
2026-08-27 15:46:55 +0530

    exercise

---
+++ C learning/27aug.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+    int main(){
+        int a,b;
+        printf("input the two numbers:");
+        scanf("%d" "%d",&a,&b);
+        printf("%d",a+b);
+        printf("  \\\\addition\n");
+        printf("%d",a-b);
+        printf("  \\\\subtraction\n");
+        printf("%d",a*b);
+        printf("  \\\\multiplication\n");
+        printf("%d",a/b);
+        printf("  \\\\division\n");
+        printf("%d",a%b);
+        printf("  \\\\modulus\n");
+        return 0;
+    }
+    
