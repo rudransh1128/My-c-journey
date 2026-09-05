5a01233cdef0d863c9aca015bb36648fedbd8725
Rudransh Gupta <rudranshgupta11728@gmail.com>
2026-09-05 23:27:34 +0530

    swap without third variable

---
+++ C learning/exercise5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    //this code replaces the values stored in varibles without using a third variable
+    printf("this is first after swap:%d\n",a);
+    printf("this is second after swap:%d\n",b);
+    return 0;
+}
