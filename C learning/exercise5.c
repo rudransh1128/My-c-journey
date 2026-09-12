8d5899d062dcabeebd4c7b31d024ab6cf99c5cde
Rudransh Gupta <rudranshgupta11728@gmail.com>
2026-09-12 08:36:20 +0530

    update

--- C learning/exercise5.c
+++ C learning/exercise5.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main(){
     int a,b;
+    printf("Enter the first number :");
     scanf("%d",&a);
+    printf("enter the second number :");
     scanf("%d",&b);
     a=a+b;
     b=a-b;
@@ -10,5 +12,8 @@ int main(){
      varibles without using a third variable*/
     printf("this is first after swap:%d\n",a);
     printf("this is second after swap:%d\n",b);
+    /*this also changes the values of variable in 2 variable
+    without using a third variable which negates the use of
+    the extra memory which is helpul to us*/
     return 0;
 }
