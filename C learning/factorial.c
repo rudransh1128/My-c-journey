f88b0113b4da572a54564456fbcaf0d760be46df
Rudransh Gupta <rudranshgupta11728@gmail.com>
2026-09-17 21:13:25 +0530

    update

--- C learning/factorial.c
+++ C learning/factorial.c
@@ -2,8 +2,9 @@
 #include <math.h>
 int main(){
     int a;
+    printf("Input the number of which the factorial is required: ");
     scanf("%d",&a);
     printf("factorial of %d :%.0f",a, tgamma(a+1));
-
+ //this program is used to find factorial of any number that is input by the user
     return 0;
 }
