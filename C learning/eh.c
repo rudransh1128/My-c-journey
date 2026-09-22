7d8165143f465e3031f612b87bd8f8af42ba799d
Rudransh Gupta <rudranshgupta11728@gmail.com>
2026-09-23 00:24:57 +0530

    .

--- C learning/eh.c
+++ C learning/eh.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+//This library helps us use rand
 #include <time.h>
+//this library helps us use time(null) to link seed generation to current time
 int main(){
     int a,b;
     srand(time(NULL));
-    b=rand() %100 + 1;
+/* Previous line of code makes sure the random number generated is different
+ by using a seed linked to the current time.*/
+     b=rand() %100 + 1;
     printf("Guess a random number between 1-100 : ");
     scanf("%d",&a);
     if(a==b)
@@ -13,4 +17,4 @@ int main(){
     printf("Incorrect guess , The correct number was %d",b);
 
     return 0;
-}
+}    
